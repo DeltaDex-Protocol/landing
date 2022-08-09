0510a070dbfa05bd9c4520d15debaeec1905f7a3
partylikeits1983 <aleqvids@gmail.com>
2022-08-09 14:17:36 +0300

    options

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../partials/Header';
-import HeroHome from '../partials/HeroHome';
-import FeaturesHome from '../partials/Features';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
-import Testimonials from '../partials/Testimonials';
-import Newsletter from '../partials/Newsletter';
-import Footer from '../partials/Footer';
+import Header from "../partials/Header";
+import HeroHome from "../partials/1stpage";
+import FeaturesHome from "../partials/3rdpage";
+import FeaturesBlocks from "../partials/FeaturesBlocks";
+import Testimonials from "../partials/Testimonials";
+import Newsletter from "../partials/Newsletter";
+import Footer from "../partials/Footer";
 
 function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-
       {/*  Site header */}
       <Header />
 
       {/*  Page content */}
       <main className="flex-grow">
-
         {/*  Page sections */}
         <HeroHome />
+        <FeaturesHome />
         <FeaturesHome />
         <FeaturesBlocks />
         <Testimonials />
         <Newsletter />
-
       </main>
 
       {/*  Site footer */}
       <Footer />
-
     </div>
   );
 }
