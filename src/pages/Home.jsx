5e60e9e3b6390d51ca2a1ce2d0e5d0e04a9ad252
partylikeits1983 <aleqvids@gmail.com>
2022-08-09 16:55:43 +0300

    file rename

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../partials/Header";
 import HeroHome from "../partials/1stblock";
 import FeaturesHome from "../partials/3rdblock";
 import FeaturesBlocks from "../partials/FeaturesBlocks";
-import Testimonials from "../partials/Testimonials";
+import Testimonials from "../partials/2ndblock";
 import Newsletter from "../partials/Newsletter";
 import Footer from "../partials/Footer";
 
