2f98c0e67d907ab444145e34f5bcfd83c23e482f
Alexander Lee <alexanderlee@Alexanders-MacBook-Pro.local>
2022-08-09 23:19:46 +0300

    path

--- src/App.jsx
+++ src/App.jsx
@@ -37,10 +37,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route exact path="" element={<Home />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="reset-password" element={<ResetPassword />} />
       </Routes>
     </>
   );
